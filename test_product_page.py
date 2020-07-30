57f40044eef27896c1f1a7ba89c06d19ce164ea4
Peter-Bud <Budzinskuy_petro@i.ua>
2020-07-30 23:17:43 +0300

    parametrize link added

--- test_product_page.py
+++ test_product_page.py
@@ -7,33 +7,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import math
 from selenium.common.exceptions import NoAlertPresentException
+import pytest
 
 
-"""def inna():
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    browser = webdriver.Chrome()
-    browser.get(link)
-    input0 = browser.find_element(By.ID, 'add_to_basket_form')
-    input0.click()
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                                marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
 
-    alert = browser.switch_to.alert
-    x = alert.text.split(" ")[2]
-    answer = str(math.log(abs((12 * math.sin(float(x))))))
-    alert.send_keys(answer)
-    alert.accept()
-    time.sleep(10)
-    try:
-        alert = self.browser.switch_to.alert
-        alert_text = alert.text
-        print(f"Your code: {alert_text}")
-        alert.accept()
-    except NoAlertPresentException:
-        print("No second alert presented")"""
-
-
-def test_add_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear20199"
+def test_add_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_button_click()
